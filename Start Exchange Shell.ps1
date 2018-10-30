fce0d8d3d62a4deb28b235d4c02906002b0d4585
HanSolo71 <HanSolo552@gmail.com>
2018-10-30 11:18:16 -0400

    Add files via upload

---
+++ Start Exchange Shell.ps1
@@ -0,0 +1,4 @@
+﻿$ExchangeServer = 'exchange.corp.com'
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchangeServer/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+Import-PSSession $Session -DisableNameChecking
